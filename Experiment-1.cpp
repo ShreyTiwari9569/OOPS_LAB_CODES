b6ab50cb18bbf0d7140c19b60f84573de9a1a4e4
Shrey Tiwari <shreytiwari884@gmail.com>
2026-05-07 18:20:46 +0530

    Update Experiment-1.cpp

--- Experiment-1.cpp
+++ Experiment-1.cpp
@@ -1,44 +1,46 @@
-
 #include <iostream>
 using namespace std;
 
-inline int add(int a, int b)
+class Calculator
 {
-    return a + b;
-}
+public:
+    inline int add(int a, int b)
+    {
+        return a + b;
+    }
 
-inline int subtract(int a, int b)
-{
-    return a - b;
-}
+    inline int subtract(int a, int b)
+    {
+        return a - b;
+    }
 
-inline int multiply(int a, int b)
-{
-    return a * b;
-}
+    inline int multiply(int a, int b)
+    {
+        return a * b;
+    }
 
-inline float divide(int a, int b)
-{
-    return (float)a / b;
-}
+    inline float divide(int a, int b)
+    {
+        return (float)a / b;
+    }
+};
 
 int main()
 {
     int a, b;
+    Calculator c;
 
     cout << "Enter two numbers: ";
     cin >> a >> b;
 
-    cout << "Addition = " << add(a, b) << endl;
-    cout << "Subtraction = " << subtract(a, b) << endl;
-    cout << "Multiplication = " << multiply(a, b) << endl;
+    cout << "Addition = " << c.add(a, b) << endl;
+    cout << "Subtraction = " << c.subtract(a, b) << endl;
+    cout << "Multiplication = " << c.multiply(a, b) << endl;
 
     if (b != 0)
-        cout << "Division = " << divide(a, b) << endl;
+        cout << "Division = " << c.divide(a, b) << endl;
     else
         cout << "Division by zero is not possible." << endl;
 
     return 0;
 }
-
-
